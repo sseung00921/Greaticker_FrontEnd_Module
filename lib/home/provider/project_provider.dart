d7ff4d68054ad8fcbb1ace09e920e8333a6db8c4
sangseunglee <sseung00921@naver.com>
2024-08-29 16:47:34 +0900

    revise projectProvider to notify to other related Providers.

--- lib/home/provider/project_provider.dart
+++ lib/home/provider/project_provider.dart
@@ -4,6 +4,7 @@ import 'package:greaticker/common/constants/language/comment.dart';
 import 'package:greaticker/common/constants/language/common.dart';
 import 'package:greaticker/common/constants/pagenation.dart';
 import 'package:greaticker/common/model/api_response.dart';
+import 'package:greaticker/diary/provider/diary_provider.dart';
 import 'package:greaticker/hall_of_fame/provider/hall_of_fame_api_response_provider.dart';
 import 'package:greaticker/history/provider/history_provider.dart';
 import 'package:greaticker/home/model/project_model.dart';
@@ -11,12 +12,15 @@ import 'package:greaticker/home/provider/got_sticker_provider.dart';
 import 'package:greaticker/home/provider/project_api_response_provider.dart';
 import 'package:greaticker/home/repository/mock_project_repository.dart';
 import 'package:greaticker/home/repository/project_repository.dart';
+import 'package:greaticker/popular_chart/provider/popular_chart_provider.dart';
 
 final projectProvider =
 StateNotifierProvider<ProjectStateNotifier, ApiResponseBase>((ref) {
   final repo = ref.watch(ProjectRepositoryProvider);
   final notifier = ProjectStateNotifier(repository: repo);
   final historyNotifier = ref.read(historyProvider.notifier);
+  final diaryNotifier = ref.read(diaryProvider.notifier);
+  final popularChartNotifier = ref.read(popularChartProvider.notifier);
 
   ref.listen<ApiResponseBase>(gotStickerProvider, (previous, next) {
     if (next is ApiResponseLoading) {
@@ -26,6 +30,8 @@ StateNotifierProvider<ProjectStateNotifier, ApiResponseBase>((ref) {
     } else {
       notifier.getProjectModel();
       historyNotifier.paginate(forceRefetch: true);
+      diaryNotifier.getDiaryModel();
+      popularChartNotifier.paginate(forceRefetch: true);
     };
   });
 
@@ -37,6 +43,8 @@ StateNotifierProvider<ProjectStateNotifier, ApiResponseBase>((ref) {
     } else {
       notifier.getProjectModel();
       historyNotifier.paginate(forceRefetch: true);
+      diaryNotifier.getDiaryModel();
+      popularChartNotifier.paginate(forceRefetch: true);
     };
   });
 
