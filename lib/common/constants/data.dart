3be9231cfcc6f92f873ba10bdfd67f78c1b82477
sangseunglee <sseung00921@naver.com>
2024-09-08 00:39:53 +0900

    hide apigateway ip.

--- lib/common/constants/data.dart
+++ lib/common/constants/data.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:greaticker/common/constants/dot_env_keys.dart';
 
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
@@ -11,4 +13,4 @@ const emulatorIp = '10.0.2.2:8080';
 const simulatorIp = '127.0.0.1:8080';
 
 //final ip = Platform.isIOS ? simulatorIp : emulatorIp;
-final ip = 'w1ait5zcb8.execute-api.ap-northeast-2.amazonaws.com/GreatickerApiStage/';
+final ip = dotenv.get(API_GATEWAY_STAGE_IP);
